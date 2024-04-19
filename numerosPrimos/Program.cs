505809e9cc2e294050f7546d03fce6d86e9e80f2
Luan-art <luanlf2003@gmail.com>
2024-04-19 15:30:46 -0300

    Add project files.

---
+++ numerosPrimos/Program.cs
@@ -0,0 +1,23 @@
+﻿int numero = 0, controle = 0;
+
+Console.WriteLine("Digite um número para que descobrirmos se ele é primo: ");
+numero = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= numero; i++){
+  
+    if (numero % i == 0)
+    {
+        controle++;
+    }
+}
+
+if (controle == 2)
+{
+    Console.WriteLine($"o numero {numero} é primo");
+
+}
+else
+{
+    Console.WriteLine("Não é primo");
+}
+
